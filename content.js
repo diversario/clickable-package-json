88d69358e0c03e1e6c8d55e48a2a748a63b02ca4
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2015-12-19 16:39:58 -0800

    Make code less dumb, support more dependencies

--- content.js
+++ content.js
@@ -8,69 +8,23 @@ function getDepName(el) {
   return match && match[1]
 }
 
+var block_start = /^\s*"(:?dev|peer|optional|bundled)?[dD]ependencies"\s*:\s*\{\s*$/
+  , block_end = /}/
+
 function linkify() {
   var lines = $('table.js-file-line-container td')
 
   if (!lines.length) return
 
-  var start_dep
-    , end_dep
-    , start_dev_dep
-    , end_dev_dep
-    , start_opt_dep
-    , end_opt_dep
-
   for (var line = 0; line < lines.length; line++) {
-    var line_text = lines[line].innerText
+    if (block_start.test(lines[line].innerText)) {
+      while (line < lines.length && !block_end.test(lines[++line].innerText)) {
+        var dep = getDepName(lines[line])
 
-    if (!start_dep && /^\s*"dependencies"\s*:\s*\{\s*$/.test(line_text)) {
-      start_dep = line
-    }
-
-    if (!start_dev_dep && /^\s*"devDependencies"\s*:\s*\{\s*$/.test(line_text)) {
-      start_dev_dep = line
-    }
-
-    if (!start_opt_dep && /^\s*"optionalDependencies"\s*:\s*\{\s*$/.test(line_text)) {
-      start_opt_dep = line
-    }
-
-    if (/}/.test(line_text)) {
-      if (start_dep && !end_dep) {
-        end_dep = line
-      } else if (start_dev_dep && !end_dev_dep) {
-        end_dev_dep = line
-      } else if (start_opt_dep && !end_opt_dep) {
-        end_opt_dep = line
+        if (dep) {
+          makeLink(lines[line], dep)
+        }
       }
-    }
-
-    if (start_dep && end_dep && start_dev_dep && end_dev_dep) {
-      break
-    }
-  }
-
-  for (line = start_dep + 1; line < end_dep; line++) {
-    var dep = getDepName(lines[line])
-
-    if (dep) {
-      makeLink(lines[line], dep)
-    }
-  }
-
-  for (line = start_dev_dep + 1; line < end_dev_dep; line++) {
-    dep = getDepName(lines[line])
-
-    if (dep) {
-      makeLink(lines[line], dep)
-    }
-  }
-
-  for (line = start_opt_dep + 1; line < end_opt_dep; line++) {
-    dep = getDepName(lines[line])
-
-    if (dep) {
-      makeLink(lines[line], dep)
     }
   }
 }
