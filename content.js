5fdc860e57a4531d6b7bc18230486bfea7b1eec2
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2015-12-19 18:12:27 -0800

    More bugfixing

--- content.js
+++ content.js
@@ -1,17 +1,24 @@
 var block_start = /^\s*"(:?dev|peer|optional|bundled)?[dD]ependencies"\s*:\s*\{\s*$/
   , block_end = /}/
-  , is_git_link = /^\s*".*"\s*:\s*"git:\/\/.*"\s*,?\s*$/g
+  , is_git_link = /^\s*".*"\s*:\s*"git:\/\/.*"\s*,?\s*$/
   , get_git_url = /git:\/\/(.*)\.git.*?/
 
 function makeLink(el, dep_name) {
   var line_text = el.innerText
     , link
 
-  if (is_git_link.test(line_text)) {
+  if (is_git_link.test(line_text) || line_text.split('/').length == 2 ) {
     var match = line_text.match(get_git_url)
 
     if (match && match[1]) {
       link = 'https://' + match[1]
+    } else { // maybe it's a "user/repo" format
+      try {
+        var parts = line_text.split(':')[1].trim().split('"')[1].split('/')
+        var username = parts[0]
+        var repo = parts[1].split('#')[0]
+        link = 'https://github.com/' + username + '/' + repo
+      } catch(e) {}
     }
   } else {
     link = '"https://npmjs.com/package/' + dep_name + '"'
